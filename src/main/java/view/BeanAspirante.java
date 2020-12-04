bef48081227ed81dd00396df9bbdf5e58a793714
Arnold Herrera <ajherrera9810@gmail.com>
2020-12-04 00:38:40 -0500

    Se logra visualizar información Ciudades por departamento

---
+++ src/main/java/view/BeanAspirante.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import model.DTO.Aspirante;
+
+@ManagedBean
+@SessionScoped
+public class BeanAspirante {
+	private Aspirante aspirante;
+}
