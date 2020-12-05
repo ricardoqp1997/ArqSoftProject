1a92a2c77df75d5ecc8762051df63974dea39558
Arnold Herrera <ajherrera9810@gmail.com>
2020-12-05 12:08:04 -0500

    Se añade consulta aspirantes

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# SoftwareEngineeringProj
+Repository for Software Engineering's Project for Ricardo Quintana - 625634
--- src/main/java/view/BeanAspirante.java
+++ src/main/java/view/BeanAspirante.java
@@ -1,22 +1,45 @@
 package view;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.model.SelectItem;
-import javax.faces.bean.SessionScoped;
 
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
+
+import controller.ControladorAspirante;
 import model.DTO.Aspirante;
 
 @ManagedBean
 @SessionScoped
 public class BeanAspirante {
-	
+	private static final String CODIGOERROR = "BEA0";
+	private static final String DESCRIPCIONERROR = " Error en bean Aspirante ";
+	private static final String MENSAJEEXITOSO = "Transaccion Exitosa ";
+	private static String codError = CODIGOERROR;
+	private static String descError = DESCRIPCIONERROR;
+	private static String mensajeError = "";
+
 	private Aspirante aspirante;
+	private List<SelectItem> aspirantes;
 	private String documento;
-	
+	private String codigo;
+
+	private ControladorAspirante controladorAspirante;
+
+	public BeanAspirante() {
+		controladorAspirante = new ControladorAspirante();
+		aspirante = new Aspirante();
+	}
+
+	public Aspirante getAspirante() {
+		return aspirante;
+	}
+
+	public void setAspirante(Aspirante aspirante) {
+		this.aspirante = aspirante;
+	}
+
 	public String getDocumento() {
 		return documento;
 	}
@@ -33,35 +56,17 @@ public class BeanAspirante {
 		this.codigo = codigo;
 	}
 
-
-
-	private String codigo;
-	public Aspirante getAspirante() {
-		return aspirante;
+	public List<SelectItem> getAspirantes() {
+		return aspirantes;
 	}
 
-	public void setAspirante(Aspirante aspirante) {
-		this.aspirante = aspirante;
+	public void setAspirantes(List<SelectItem> aspirantes) {
+		this.aspirantes = aspirantes;
 	}
-	
-	
 
-	private static final String CODIGOERROR = "BED0";
-	private static final String DESCRIPCIONERROR = " Error en bean Departamento ";
-	private static final String MENSAJEEXITOSO = "Transaccion Exitosa ";
-	private static String codError = CODIGOERROR;
-	private static String descError = DESCRIPCIONERROR;
-	private static String mensajeError = "";
-	
-	public BeanAspirante(){
-		aspirante = new Aspirante();
+	public void consultarAspirante() {
+		aspirante = controladorAspirante.consultarAspirante(Integer.parseInt(this.documento));
+
 	}
-	
-	public void getDatosAspirante() {
-		
-	}
-	
-	
 
 }
-
