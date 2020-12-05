11b63a7ccec1b9d4e10debd391e6e84b978bf1c3
ricardoqp1997 <69210936+ricardoqp1997@users.noreply.github.com>
2020-12-05 07:53:26 -0500

    primeros cambios en resultado de estudiantes

--- src/main/java/view/BeanAspirante.java
+++ src/main/java/view/BeanAspirante.java
@@ -9,11 +9,48 @@ import model.DTO.Aspirante;
 @SessionScoped
 public class BeanAspirante {
 	private Aspirante aspirante;
+	private String documento;
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+	private String codigo;
+	public Aspirante getAspirante() {
+		return aspirante;
+	}
+
+	public void setAspirante(Aspirante aspirante) {
+		this.aspirante = aspirante;
+	}
+	
+	
+
 	private static final String CODIGOERROR = "BED0";
 	private static final String DESCRIPCIONERROR = " Error en bean Departamento ";
 	private static final String MENSAJEEXITOSO = "Transaccion Exitosa ";
 	private static String codError = CODIGOERROR;
 	private static String descError = DESCRIPCIONERROR;
 	private static String mensajeError = "";
+	
+	public BeanAspirante(){
+		aspirante = new Aspirante();
+	}
+	
+	
 
 }
+
