342934661994d70b2a6d2e52b7743edcc5c89fdf
Arnold Herrera <ajherrera9810@gmail.com>
2020-12-05 12:31:44 -0500

    Se corrige consulta aspirantes

--- src/main/java/view/BeanAspirante.java
+++ src/main/java/view/BeanAspirante.java
@@ -90,7 +90,11 @@ public class BeanAspirante {
 	}
 
 	public void consultarAspirante() {
-		aspirante = controladorAspirante.consultarAspirante(Integer.parseInt(this.documento));
+
+		if (controladorAspirante.consultarAspirante(Integer.parseInt(this.documento)).getQrcodeAspirante()
+				.toString() == codigo) {
+			aspirante = controladorAspirante.consultarAspirante(Integer.parseInt(this.documento));
+		}
 
 	}
 
